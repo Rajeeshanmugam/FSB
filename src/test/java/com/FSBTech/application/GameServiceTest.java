58ccc10518ed1e19c8a0bd5bad43a367c218ff68
Rajeeshanmugam <36992089+Rajeeshanmugam@users.noreply.github.com>
2023-07-24 11:45:35 +0100

    Update GameServiceTest.java

--- src/test/java/com/FSBTech/application/GameServiceTest.java
+++ src/test/java/com/FSBTech/application/GameServiceTest.java
@@ -1,51 +1,34 @@
 package com.FSBTech.application;
 
-import com.FSBTech.application.dto.GameDto;
 import com.FSBTech.application.entity.Game;
 import com.FSBTech.application.repository.GameServiceRepository;
 import com.FSBTech.application.service.GameServiceImplementation;
-import jakarta.inject.Inject;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 
+@SpringBootTest
 class GameServiceTest {
 
-	@Mock
+	@MockBean
 	GameServiceRepository repository;
 
-	@InjectMocks
+	@Autowired
 	GameServiceImplementation service;
 	@Test
 	void addGame() {
-		// Arrange
-		final var gameToSave = Game.builder().name("Mary").active(true).build();
+		// given
+		final var gameToSave = Game.builder().gameId(123L).name("Mary").dateOfCreation("2020-10-10").active(true).build();
 		when(repository.save(any(Game.class))).thenReturn(gameToSave);
-
-		// Act
-		final var gameToAdd = GameDto.builder().name("Mary").active(true).build();
-		final var actual = service.addGame(gameToAdd);
-
+		Game gameResult = repository.save(gameToSave);
 		// Assert
-		assertThat(actual).usingRecursiveComparison().isEqualTo(gameToSave);
-		verify(repository, times(1)).save(any(Game.class));
-		verifyNoMoreInteractions(repository);
+		assertThat(gameResult).usingRecursiveComparison().isEqualTo(gameToSave);
 	}
-
 
 }
